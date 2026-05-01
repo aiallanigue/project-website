9ec1c029364c9a4a75aadfa5be9e3aa790a265cd
LAN-PC\63999 <allanallanigue@gmail.com>
2026-05-02 00:48:22 +0800

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/project-website',
+  assetPrefix: '/project-website/',
+  trailingSlash: true,
 };
 
 export default nextConfig;
